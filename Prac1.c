04b808939a1e3143ec977ed3284038267d633d0f
Roberto Francisco Sánchez Dávila <99695424+sdavila4@users.noreply.github.com>
2025-02-05 11:16:03 -0800

    Prac1.c

--- Prac1.c
+++ Prac1.c
@@ -1,7 +1,3 @@
-/******************************************************************************
-Examples code
-*******************************************************************************/
-
 #include <stdio.h>
 #include <inttypes.h>
 /*
@@ -30,13 +26,41 @@ int main()
     a |= (1<<15) | (3<<12) | (1<<7) | (3<<4);
     printf("\na = %X", a);
     
-//2 BABA
-//3 BEBE
-//4 FE00
+//2 BABA  1011 0000 1011 0000 (valor anterior)
+//        1011 1010 1011 1010 (valor que se desea)
+    a |= (5<<9) | (5<<1);
+    printf("\na = %X", a);
+//3 BEBE  1011 1010 1011 1010 (valor anterior)
+//        1011 1110 1011 1110 (valor que se desea)
+    a |= (1<<10) | (1<<7) | (3<<4)| (14<<1);
+    printf("\na = %X", a);
+//4 FE00 
+//        1011 1110 1011 1110 (valor anterior)
+//        1111 1110 0000 0000 (valor que se desea)
+    a ^= (1<<14) ^ (1<<7) ^ (3<<4) ^ (7<<1);
+    printf("\na = %X", a);
 //5 FEAA
+//        1111 1110 0000 0000 (valor anterior)
+//        1111 1110 1010 1010 (valor que se desea)
+    a |= (10<<4) | (10<<0);
+    printf("\na = %X", a);
 //6 0155
+//        1111 1110 1010 1010 (valor anterior)
+//        0000 0001 0101 0101 (valor que se desea)
+        a ^= (15<<12) ^ (15<<8) ^ (15<<4) ^ (15<<0);
+        printf("\na = %X", a);
+
 //7 0001
+//        0000 0001 0101 0101 (valor anterior)
+//        0000 0000 0000 0001 (valor que se desea)
+    a ^= (1<<8) ^ (5<<4) ^ (1<<2);
+    printf("\na = %X", a);
+
 //8 0100
+//        0000 0000 0000 0001 (valor anterior)
+//        0000 00001 0000 0000 (valor que se desea)
+    a ^= (1<<8) ^ (1<<0); 
+    printf("\na = %X", a);
 
     return 0;
 }
